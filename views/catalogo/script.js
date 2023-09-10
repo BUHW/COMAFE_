a4be8c581f3bfb60446654f3ba75a9e34dd4a9f5
Victor Antonio <victorapereira444013@gmail.com>
2023-09-10 01:41:00 -0300

    versao 0.0.7

--- views/catalogo/script.js
+++ views/catalogo/script.js
@@ -1,41 +1,131 @@
-// // Janela modal
+// Janela modal
 
-// function abrirModal(){
-//     const modal = document.getElementById('janela-modal')
-//     modal.classList.add('abrir')
+function abrirModal(){
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
 
-//     modal.addEventListener('click', (e) => {
-//         if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
-//             modal.classList.remove('abrir')
-//         }
-//     })
-// }
-
-function abrirModal(targetId) {
-    const modal = document.getElementById(targetId);
-    modal.classList.add('abrir');
-
-    
-    // Adicione um ouvinte de evento de clique ao botão de fechar dentro da modal
-    const fecharBotao = modal.querySelector('.fechar-modal');
-    fecharBotao.addEventListener('click', () => {
-        modal.classList.remove('abrir');
-    });
-
-    // Centralizar a janela modal em relação ao botão clicado
-    const button = document.querySelector(`[data-target="${targetId}"]`);
-    const rect = button.getBoundingClientRect();
-    modal.style.top = `${rect.top + window.scrollY}px`;
-    modal.style.left = `${rect.left + window.scrollX + (rect.width - modal.offsetWidth) / 2}px`;
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar' || e.target.id == 'janela-modal'){
+            modal.classList.remove('abrir')
+        }
+    })
 }
-console.log('data-target')
 
-// Adicione um ouvinte de evento de clique a todos os botões "Tenho interesse"
-const botoesInteresse = document.querySelectorAll('.btn');
-botoesInteresse.forEach(botao => {
-    botao.addEventListener('click', () => {
-        const targetId = botao.getAttribute('data-target');
-        abrirModal(targetId); // Certifique-se de passar o ID correto aqui
-        console.log(targetID)
+function abrirModal1(){
+    const modal = document.getElementById('janela-modal1')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar1' || e.target.id == 'janela-modal1'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal2(){
+    const modal = document.getElementById('janela-modal2')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar2' || e.target.id == 'janela-modal2'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal3(){
+    const modal = document.getElementById('janela-modal3')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar3' || e.target.id == 'janela-modal3'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal4(){
+    const modal = document.getElementById('janela-modal4')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar4' || e.target.id == 'janela-modal4'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal5(){
+    const modal = document.getElementById('janela-modal5')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar5' || e.target.id == 'janela-modal5'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal6(){
+    const modal = document.getElementById('janela-modal6')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar6' || e.target.id == 'janela-modal6'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal7(){
+    const modal = document.getElementById('janela-modal7')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar7' || e.target.id == 'janela-modal7'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+function abrirModal8(){
+    const modal = document.getElementById('janela-modal8')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar8' || e.target.id == 'janela-modal8'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
+
+// Btn buscar
+
+// Função para pesquisar os itens do catálogo
+const inputPesquisa = document.getElementById('search');
+const btnBusca = document.getElementById('btnBusca');
+
+function pesquisarCatalogo() {
+    const termoPesquisa = inputPesquisa.value.toLowerCase();
+    const catalogItems = document.querySelectorAll('.card-item');
+
+    catalogItems.forEach(item => {
+        const titulo = item.querySelector('h3').textContent.toLowerCase();
+
+        if (titulo.includes(termoPesquisa)) {
+            item.classList.remove('oculto');
+        } else {
+            item.classList.add('oculto');
+        }
     });
+}
+
+btnBusca.addEventListener('click', pesquisarCatalogo);
+
+// Adicione um ouvinte de evento para capturar a tecla Enter pressionada
+inputPesquisa.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        pesquisarCatalogo();
+    }
 });
+
